3764e4a8b9a7e3458ba62a323b6e629306a28d17
Darian Moody <mail@djm.org.uk>
2010-12-08 21:29:41 +0000

    Added a setup.py and set up structure to make install via pip simple.

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+f = open(os.path.join(os.path.dirname(__file__), 'README'))
+readme = f.read()
+f.close()
+
+setup(
+        name='django-syntax-colorize',
+        version='0.1',
+        description='A django app that provides a filters for working with syntax' \
+                'highlighting via the Pygments library.',
+        long_description=readme,
+        author='Will Larson',
+        author_email='lethain@gmail.com',
+        url='https://github.com/lethain/django-syntax-colorize',
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=['pygments'],
+        classifiers=[
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Framework :: Django',
+            ],
+        )
+
